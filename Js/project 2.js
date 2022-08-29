284c2ee64bf241f65859991406645f2e6c25f19e
Amin Wassef <dr.amin.wassef@gmail.com>
2022-08-30 00:41:24 +0300

    Main Folder Created

---
+++ Js/project 2.js
@@ -0,0 +1,7 @@
+var light = document.getElementById("light")
+var body = document.getElementById("body")
+
+function visible(){
+    light.classList.toggle("on");
+    body.classList.toggle("change-color");
+}
